3a6c277224c884d0d716f06a8031e20c68b08927
Yuriy Ivanov <ymivanov@icloud.com>
2020-04-19 18:33:51 +0300

    typescript npm scripts options, nodemon installation

--- build/server.js
+++ build/server.js
@@ -3,11 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var http_1 = __importDefault(require("http"));
-var app = express_1.default();
-var PORT = process.env.PORT ? Number(process.env.PORT) : 8080;
-var server = http_1.default.createServer(app);
-server.listen(PORT, function () {
-    console.log("Listening at PORT: " + PORT);
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const app = express_1.default();
+const PORT = process.env.PORT ? Number(process.env.PORT) : 8080;
+const server = http_1.default.createServer(app);
+server.listen(PORT, () => {
+    console.log(`Listening at PORT: ${PORT}`);
 });
+//# sourceMappingURL=server.js.map
