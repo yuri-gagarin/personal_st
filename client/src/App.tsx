6501385a2539a78f787395487a0230b102623084
Yuriy Ivanov <ymivanov@icloud.com>
2020-04-20 12:18:40 +0300

    bezels for the screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import HomeScreenComponent from "./components/HomeScreenComponent";
+import HomeScreenControlls from "./components/HomeScreenControlls";
+
 function App() {
   return (
     <div className="App">
       <HomeScreenComponent
         title="Home Screen"
       />
+      <HomeScreenControlls />
     </div>
   );
 }
---
+++ client/src/components/HomeScreenControlls.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomeScreenControlls.css";
+
+const HomeScreenControlls: React.SFC<{}> = (props): JSX.Element => {
+  return (
+    <div id="crtControls">
+      <div id="crtPowerButton">
+        Power
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreenControlls;
