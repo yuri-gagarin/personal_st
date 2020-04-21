19aa63c1c706987f8418f74bb98205cdf709b731
Yuriy Ivanov <ymivanov@icloud.com>
2020-04-21 21:51:45 +0300

    proxy to express server, sounds for power button

--- app/server.ts
+++ app/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import path from "path";
 
 // custom middleware //
 import requestLogger from "./custom_middleware/requestLogger";
@@ -18,6 +19,15 @@ if (process.env.NODE_ENV !== "production") {
 combinedRoutes(Router);
 app.use(Router);
 // end router and routes configuration //
+// staic files configuration //
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "public")));
+} else {
+  app.use(express.static(path.join(__dirname, "client/src")));
+  app.use(express.static(path.join(__dirname, "public")));
+}
+// end static files configuration //
 
 server.listen(PORT, () => {
   console.log(`Listening at PORT: ${PORT}`);
--- client/src/components/home_screen/HomeScreenControlls.tsx
+++ client/src/components/home_screen/HomeScreenControlls.tsx
@@ -1,14 +1,26 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import "./css/homeScreenControlls.css";
 // additional components //
 import PowerSwitchComponent from "./PowerSwitchComponent";
 
 const HomeScreenControlls: FC<{}> = (props): JSX.Element => {
   const [powerOn, setPowerOn] = useState(true);
+  const [powerButtonSound, setPowerButtonSound] = useState<HTMLAudioElement>();
 
   const togglePowerSwitch = (): void => {
-    setPowerOn(!powerOn);
-  }
+    if (powerButtonSound) {
+      powerButtonSound.play();
+      setPowerOn(!powerOn);
+    } else {
+      setPowerOn(!powerOn);
+    }
+  };
+  // lifecycle hooks //
+  useEffect(() => {
+    const powerButtonSound: HTMLAudioElement = new Audio("./../../media/sounds/keys/power_button.mp3");
+    console.log(powerButtonSound);
+    setPowerButtonSound(powerButtonSound);
+  }, []);
 
   return (
     <div id="crtControls">
