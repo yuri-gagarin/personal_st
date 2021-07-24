7d538582854252dbd4e0cd2f1063758772aef9a1
Yuriy Ivanov <ymivanov@icloud.com>
2021-07-24 15:34:53 +0300

    add neon glow to <ModernClassicSwitch> button style

--- client/src/components/switches/ModernClassicSwitch.tsx
+++ client/src/components/switches/ModernClassicSwitch.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React,  { useEffect, useRef, useState } from "react";
 import styles from "./css/modernClassicSwitches.module.css";
 
 export const ModernClassicSwitch = (): JSX.Element => {
+  const [ switchState, setSwitchState ] = useState<{ setting: "modern" | "classic"}>({ setting: "classic" });
+  const checkboxInput = useRef<HTMLInputElement | null>(null);
 
+  const handleBtnClick = (): void => {
+    if (checkboxInput.current && checkboxInput.current.checked) {
+      setSwitchState({ setting: "classic" });
+    } else {
+      setSwitchState({ setting: "modern" });
+    }
+  };
+
+  useEffect(() => {
+    console.log(switchState);
+  }, [ switchState ]);
   return (
     <div className={ styles.switchWrapper }>
       <label className={ styles.switchRocker }>
-        <input type="checkbox" ></input>
-        <span className={ styles.switchLeft }>Modern</span>
-        <span className={ styles.switchRight }>Classic</span>
+        <input ref={ checkboxInput } type="checkbox" ></input>
+        <span className={ styles.switchLeft } onClick={ handleBtnClick }>Modern</span>
+        <span className={ `${styles.switchRight} ${ switchState.setting === "classic" ? styles.classicText : ""}` } onClick={ handleBtnClick }>Classic</span>
       </label>
     </div>
   );
